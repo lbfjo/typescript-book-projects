a2251c5770656e1e8ba825202077f13b0625bdff
Francisco Bruno <franciscobruno@MBPdeFrancisco.lan>
2022-11-16 15:04:58 +0000

    generics exercices 2

--- projects/generics/hidash/02-zip/index.ts
+++ projects/generics/hidash/02-zip/index.ts
@@ -1,2 +1,17 @@
 // Write your zip function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function zip<T, Y>(a: T[], b: Y[]) {
+	let arrayOfResults: (T | Y)[] = [];
+	let i: number;
+
+	for (i = 0; i < Math.min(a.length, b.length); i += 1) {
+		arrayOfResults.push(a[i]);
+		arrayOfResults.push(b[i]);
+	}
+	for (const remaining of [a, b]) {
+		for (; i < remaining.length; i += 1) {
+			arrayOfResults.push(remaining[i]);
+		}
+	}
+}
